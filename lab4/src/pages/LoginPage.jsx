e7604638ceee062951b69ac0b0f53c6efe27aa14
Adwersity <maksym.dubyk1@gmail.com>
2025-05-06 21:02:44 +0300

    update

--- lab4/src/pages/LoginPage.jsx
+++ lab4/src/pages/LoginPage.jsx
@@ -3,13 +3,6 @@ import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
 import { useNavigate } from 'react-router-dom';
 
-
-const LoginPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const navigate = useNavigate();
-
   const getErrorMessage = (code) => {
     switch (code) {
       case 'auth/invalid-email':
